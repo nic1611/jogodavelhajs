d8a73415fde06be80fdab735802a80993ca7ba0e
Nicolas <nisoffi@gmail.com>
2020-04-08 22:57:33 -0300

    Adicionando as regras de vitorias que faltavam

--- scripts.js
+++ scripts.js
@@ -87,6 +87,7 @@ function checkWinCondition() {
     let b8 = document.getElementById("block-8");
     let b9 = document.getElementById("block-9");
 
+    // horizontal
     if (b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
         let b1Child = b1.childNodes[0].className;
         let b2Child = b2.childNodes[0].className;
@@ -99,7 +100,106 @@ function checkWinCondition() {
             // o
             declareWinner('o');
         }
+    }
 
+    if (b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length > 0) {
+        let b4Child = b4.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+
+        if (b4Child == 'x' && b5Child == 'x' && b6Child == 'x') {
+            // x
+            declareWinner('x');
+        } else if (b4Child == 'o' && b5Child == 'o' && b6Child == 'o') {
+            // o
+            declareWinner('o');
+        }
+    }
+
+    if (b7.childNodes.length > 0 && b8.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b7Child = b7.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if (b7Child == 'x' && b8Child == 'x' && b9Child == 'x') {
+            // x
+            declareWinner('x');
+        } else if (b7Child == 'o' && b8Child == 'o' && b9Child == 'o') {
+            // o
+            declareWinner('o');
+        }
+    }
+
+    // vertical
+    if (b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0) {
+        let b1Child = b1.childNodes[0].className;
+        let b4Child = b4.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+
+        if (b1Child == 'x' && b4Child == 'x' && b7Child == 'x') {
+            // x
+            declareWinner('x');
+        } else if (b1Child == 'o' && b4Child == 'o' && b7Child == 'o') {
+            // o
+            declareWinner('o');
+        }
+    }
+
+    if (b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0) {
+        let b2Child = b2.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+
+        if (b2Child == 'x' && b5Child == 'x' && b8Child == 'x') {
+            // x
+            declareWinner('x');
+        } else if (b2Child == 'o' && b5Child == 'o' && b8Child == 'o') {
+            // o
+            declareWinner('o');
+        }
+    }
+
+    if (b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b3Child = b3.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if (b3Child == 'x' && b6Child == 'x' && b9Child == 'x') {
+            // x
+            declareWinner('x');
+        } else if (b3Child == 'o' && b6Child == 'o' && b9Child == 'o') {
+            // o
+            declareWinner('o');
+        }
+    }
+
+    // diagonais
+    if (b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b1Child = b1.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if (b1Child == 'x' && b5Child == 'x' && b9Child == 'x') {
+            // x
+            declareWinner('x');
+        } else if (b1Child == 'o' && b5Child == 'o' && b9Child == 'o') {
+            // o
+            declareWinner('o');
+        }
+    }
+
+    if (b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0) {
+        let b3Child = b3.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+
+        if (b3Child == 'x' && b5Child == 'x' && b7Child == 'x') {
+            // x
+            declareWinner('x');
+        } else if (b3Child == 'o' && b5Child == 'o' && b7Child == 'o') {
+            // o
+            declareWinner('o');
+        }
     }
 
     // deu velha
